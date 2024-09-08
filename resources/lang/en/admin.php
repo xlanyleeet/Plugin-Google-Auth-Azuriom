14a068951e3fa113034e74c3d956042a366abe59
Vlad Hrytsyshyn <gricicinvlad@gmail.com>
2024-09-08 16:41:25 +0300

    add uk lang
    add uk lang

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -1,13 +1,14 @@
 <?php
 
 return [
-    'permission' => 'View and edit the settings of the Google Auth plugin',
-    'title' => 'Google Auth Settings',
-    'header' => "Authentication management through Google",
-    'subtitle' => 'Your google project',
-    'cloud-google' => 'Create a Google project',
+    'permission' => 'Переглядати і змінювати налаштування плагіну Google Auth',
+    'title' => 'Налаштування Google Auth',
+    'header' => "Вхід на  сайт за допомогою Google",
+    'subtitle' => 'Твій проєкт у Google',
+    'cloud-google' => 'Створити проєкт у Google',
     'client_id' => 'Client ID',
     'client_secret' => 'Client Secret',
-    'uri' => 'Redirection URI',
-    'support' => 'Support',
+    'uri' => 'URI переадресації',
+    'support' => 'Підтримка',
+    'max_per_ip' => 'Макс. кількість реєстрацій на IP'
 ];
